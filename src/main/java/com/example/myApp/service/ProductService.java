98d0208e7c621f1723e8608f13d482ba2e36871f
QuyenNDD <nguyenducdoanquyen@gmail.com>
2025-03-07 17:25:36 +0700

    search/paging

--- src/main/java/com/example/myApp/service/ProductService.java
+++ src/main/java/com/example/myApp/service/ProductService.java
@@ -1,11 +1,12 @@
 package com.example.myApp.service;
 
 import com.example.myApp.enity.Products;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 public interface ProductService {
-    List<Products> getAllProducts();
+    Page<Products> getAllProducts(int page, int size);
     Products getProductById(Integer id);
-    List<Products> searchProductByName(String name);
+    Page<Products> searchProductByName(String name, int page, int size);
 }
--- src/main/java/com/example/myApp/service/serviceImpl/ProductServiceImpl.java
+++ src/main/java/com/example/myApp/service/serviceImpl/ProductServiceImpl.java
@@ -3,6 +3,10 @@ package com.example.myApp.service.serviceImpl;
 import com.example.myApp.enity.Products;
 import com.example.myApp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import com.example.myApp.repository.ProductRepository;
 
@@ -15,8 +19,9 @@ public class ProductServiceImpl implements ProductService {
     private ProductRepository productRepository;
 
     @Override
-    public List<Products> getAllProducts() {
-        return productRepository.findAll();
+    public Page<Products> getAllProducts(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by("name").ascending());
+        return productRepository.findAll(pageable);
     }
 
     @Override
@@ -25,7 +30,8 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    public List<Products> searchProductByName(String name){
-        return productRepository.findByNameContaining(name);
+    public Page<Products> searchProductByName(String name, int page, int size){
+        Pageable pageable = PageRequest.of(page, size, Sort.by("name").ascending());
+        return productRepository.searchProductsByName(name, pageable);
     }
 }
