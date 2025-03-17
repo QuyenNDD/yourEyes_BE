7df73869b7036f20cde4198bb46177d6508a7c98
QuyenNDD <nguyenducdoanquyen@gmail.com>
2025-03-17 22:29:18 +0700

    nhap hang

--- src/main/java/com/example/myApp/dto/DiscountDTO.java
+++ src/main/java/com/example/myApp/dto/DiscountDTO.java
@@ -15,10 +15,13 @@ public class DiscountDTO {
     private String code;
     private String description;
     private BigDecimal discountPercentage;
-
+    private LocalDate startDate;
+    private LocalDate endDate;
     public DiscountDTO(Discount discount) {
         this.code = discount.getCode();
         this.description = discount.getDescription();
         this.discountPercentage = discount.getDiscountPercentage();
+        this.startDate = discount.getStartDate();
+        this.endDate = discount.getEndDate();
     }
 }
